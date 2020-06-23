761f08795f7c9227842656da4847b36c7c70d93f
egoing <>
2020-06-23 10:30:55 +0900

    5. e의 비밀

--- src/ExceptionApp.java
+++ src/ExceptionApp.java
@@ -4,12 +4,13 @@ public class ExceptionApp {
         int[] scores = {10,20,30};
         try {
             System.out.println(2);
-            System.out.println(scores[3]);
+//            System.out.println(scores[3]);
             System.out.println(3);
-//            System.out.println(2 / 0);
+            System.out.println(2 / 0);
             System.out.println(4);
         } catch(ArithmeticException e){
-            System.out.println("계산이 잘못된 것 같아요.");
+            System.out.println("계산이 잘못된 것 같아요."+e.getMessage());
+            e.printStackTrace();
         } catch(Exception e){
             System.out.println("먼가 이상합니다. 오류가 발생했습니다. ");
         }
