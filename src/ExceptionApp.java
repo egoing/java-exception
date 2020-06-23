b79b7234c4ceba2d4e04c7c7f347e766b0508874
egoing <>
2020-06-23 09:04:00 +0900

    4. 예외의 우선순위

--- src/ExceptionApp.java
+++ src/ExceptionApp.java
@@ -6,12 +6,12 @@ public class ExceptionApp {
             System.out.println(2);
             System.out.println(scores[3]);
             System.out.println(3);
-            System.out.println(2 / 0);
+//            System.out.println(2 / 0);
             System.out.println(4);
         } catch(ArithmeticException e){
-            System.out.println("잘못된 계산이네요.");
-        } catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("없는 값을 찾고 계시네요 ^^");
+            System.out.println("계산이 잘못된 것 같아요.");
+        } catch(Exception e){
+            System.out.println("먼가 이상합니다. 오류가 발생했습니다. ");
         }
         System.out.println(5);
     }
