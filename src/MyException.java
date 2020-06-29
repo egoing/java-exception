2c188df8047cecbb3475e8d9a6390873470b0406
egoing <>
2020-06-29 12:10:02 +0900

    수업을 마치며

---
+++ src/MyException.java
@@ -0,0 +1,8 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MyException {
+    public static void main(String[] args){
+        throw new RuntimeException("무언가 문제가 있습니다");
+    }
+}
